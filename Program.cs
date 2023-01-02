92184b7bd5e86a477560a10bd662bb454fcfe9e3
0FiFi0 <115630892+0FiFi0@users.noreply.github.com>
2023-01-02 16:44:36 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace Projekt8086
+{
+    internal static class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+
+        }
+    }
+}
