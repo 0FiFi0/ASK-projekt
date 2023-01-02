9179ffac1d795aebd949878e4d55c8e56bdae15b
0FiFi0 <115630892+0FiFi0@users.noreply.github.com>
2023-01-02 17:21:36 +0100

    Add files via upload

---
+++ 8086/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Projekt8086
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.XCHG_btn = new System.Windows.Forms.Button();
+            this.MOV_btn = new System.Windows.Forms.Button();
+            this.moV1 = new Projekt8086.MOV();
+            this.xchG1 = new Projekt8086.XCHG();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+   
+
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            this.printPreviewDialog1.Load += new System.EventHandler(this.printPreviewDialog1_Load);
+
+
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.XCHG_btn);
+            this.panel1.Controls.Add(this.MOV_btn);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(121, 311);
+            this.panel1.TabIndex = 14;
+
+
+            this.XCHG_btn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.XCHG_btn.Location = new System.Drawing.Point(21, 158);
+            this.XCHG_btn.Name = "XCHG_btn";
+            this.XCHG_btn.Size = new System.Drawing.Size(75, 23);
+            this.XCHG_btn.TabIndex = 0;
+            this.XCHG_btn.Text = "XCHG";
+            this.XCHG_btn.UseVisualStyleBackColor = true;
+            this.XCHG_btn.Click += new System.EventHandler(this.XCHG_btn_Click);
+
+
+            this.MOV_btn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.MOV_btn.Location = new System.Drawing.Point(21, 118);
+            this.MOV_btn.Name = "MOV_btn";
+            this.MOV_btn.Size = new System.Drawing.Size(75, 23);
+            this.MOV_btn.TabIndex = 0;
+            this.MOV_btn.Text = "MOV";
+            this.MOV_btn.UseVisualStyleBackColor = true;
+            this.MOV_btn.Click += new System.EventHandler(this.MOV_Click);
+
+
+            this.moV1.Location = new System.Drawing.Point(127, 0);
+            this.moV1.Name = "moVwind1";
+            this.moV1.Size = new System.Drawing.Size(460, 310);
+            this.moV1.TabIndex = 15;
+
+
+            this.xchG1.Location = new System.Drawing.Point(127, 0);
+            this.xchG1.Name = "xchGwid1";
+            this.xchG1.Size = new System.Drawing.Size(460, 310);
+            this.xchG1.TabIndex = 16;
+
+
+            this.panel2.BackColor = System.Drawing.Color.Gray;
+            this.panel2.Location = new System.Drawing.Point(0, 95);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 111);
+            this.panel2.TabIndex = 17;
+
+
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 46);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "8086";
+
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 311);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.xchG1);
+            this.Controls.Add(this.moV1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "8086 MOV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PrintPreviewDialog printPreviewDialog1;
+        private Panel panel1;
+        private Button XCHG_btn;
+        private Button MOV_btn;
+        private MOV moV1;
+        private XCHG xchG1;
+        private Label label1;
+        private Panel panel2;
+    }
+}
